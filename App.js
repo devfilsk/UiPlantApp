5c4829e7aaaf253b3785986f3b468a43467a05df
Filipe Maciel <devfilsk@gmail.com>
2020-01-09 00:30:17 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View, Text
+} from 'react-native';
+
+
+import Navigation from './App/navigation';
+import { Block } from './App/components';
+// import * as constatns './App/constants';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>
+        Teste
+      </Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
